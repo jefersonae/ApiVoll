971095829232ec27b5f9323ff4249a5e7c26f20f
Jeferson de Souza Andrade <38109358+jefersonae@users.noreply.github.com>
2024-05-21 20:09:37 -0300

    add ResponseEntity

---
+++ src/main/java/com/Api/dto/DadosDetalhamentoMedico.java
@@ -0,0 +1,12 @@
+package com.Api.dto;
+
+import com.Api.model.Endereco;
+import com.Api.model.Medico;
+
+public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
+
+    public DadosDetalhamentoMedico (Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+
+    }
+}
---
+++ src/main/java/com/Api/dto/DadosDetalhamentoPaciente.java
@@ -0,0 +1,10 @@
+package com.Api.dto;
+
+import com.Api.model.Endereco;
+import com.Api.model.Paciente;
+
+public record DadosDetalhamentoPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) {
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(), paciente.getEndereco());
+    }
+}
